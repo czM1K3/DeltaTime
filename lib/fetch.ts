c1a7b36d4d174b58fbe5bd365c0eeb929edc9e6a
czM1K3 <misamadera1@gmail.com>
2021-04-13 09:52:27 +0200

    Used map instead of forEach

--- lib/fetch.ts
+++ lib/fetch.ts
@@ -8,17 +8,15 @@ export const getData = async (classId, day, lesson) => {
         .then((x) => x.text())
         .then((text) => {
             const dom = new JSDOM(text);
-            const array = [];
-            dom.window.document
-                .querySelectorAll(
-                    "#main > div:nth-child(" +
-                        (day + 2) +
-                        ") > div.bk-cell-wrapper > div:nth-child(" +
-                        (lesson + 1) +
-                        ") > div > div > div > div.middle"
-                )
-                .forEach((x) => array.push(x.innerHTML));
-            return array;
+            const nodeList = dom.window.document.querySelectorAll(
+                "#main > div:nth-child(" +
+                    (day + 2) +
+                    ") > div.bk-cell-wrapper > div:nth-child(" +
+                    (lesson + 1) +
+                    ") > div > div > div > div.middle"
+            );
+            //@ts-ignore
+            return Array.from(nodeList).map((x) => x.innerHTML);
         })
         .catch(() => {
             return null;
