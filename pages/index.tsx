a77e390e279ae88da7580618b721f4eb4309486b
czM1K3 <misamadera1@gmail.com>
2021-04-25 14:51:26 +0200

    Tweaked some settings

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,12 @@ import Head from "next/head";
 import { useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
 import { getString, getDeltaTime } from "../lib/time";
-import { useHelloQuery } from "../lib/graphql/hello.graphql";
+import { initializeApollo } from "../lib/apollo";
+import { ClassListDocument, useClassListQuery } from "../lib/graphql/classList.graphql";
 
 const Home: FC = () => {
     const [datum, setDatum] = useState("");
-    const { data, loading, error } = useHelloQuery();
+    const { data, loading, error } = useClassListQuery();
 
     useEffect(() => {
         const interval = setInterval(() => {
@@ -22,8 +23,22 @@ const Home: FC = () => {
                 <title>{datum}</title>
                 <link rel="icon" href="/logo.ico" />
             </Head>
-            <h1 className={styles.time}>{datum} {loading ? "Loading" : error ? "Error" : data.hello}</h1>
+            <p className={styles.classes}>{loading ? "Loading" : error ? "Error" : data.timetableAll.map(x => x.label + " ").sort()}</p>
+            <h1 className={styles.time}>{datum}</h1>
         </div>
     );
 };
 export default Home;
+
+export const getStaticProps = async () => {
+    const apolloClient = initializeApollo();
+    await apolloClient.query({
+        query: ClassListDocument,
+    })
+
+    return {
+        props: {
+            initialApolloState: apolloClient.cache.extract(),
+        }
+    }
+}
